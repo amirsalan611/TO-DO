bcf944b4bb0b1d286712b00f1a6a7604b6a7aed3
arsalan poodat <amir.arsalan.poudat@gmail.com>
2024-12-19 17:11:55 +0330

    feat: create the add user section and sorted users in users box

--- script.js
+++ script.js
@@ -1,16 +1,53 @@
-const baseURL = `https://675f29cb1f7ad2426997bda6.mockapi.io/amirSalan`
-const cardsBody = document.getElementById("cards-body")
+const baseURL = `https://675f29cb1f7ad2426997bda6.mockapi.io/amirSalan`;
+const cardsBody = document.getElementById("cards-body");
 
-fetch(`${baseURL}/user`).then((response)=>{
-    return response.json();
-}).then((result)=>{
-result.forEach(item => {
-    console.log(result);
-    cardsBody.innerHTML += `<div id="card" class="flex flex-col shadow-md w-full items-center justify-center p-5">
+renderUsers();
+
+function renderUsers() {
+  fetch(`${baseURL}/user`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((result) => {
+      cardsBody.innerHTML = "";
+      const sortedResult = result.sort((a, b) => {
+        return b.id - a.id;
+      });
+      sortedResult.forEach((item) => {
+        cardsBody.innerHTML += `<div id="card" class="flex flex-col shadow-md w-full items-center justify-center p-5">
                 <img src="./assets/image/download.png" alt="">  
                 <h2 id="user-name" class="text-[20px]">${item.name}</h2>
                 <p id="nID">national ID : ${item["national-id"]}</p>
                 <p id="city">City : ${item.city}</p>
-            </div>`
-});
-})
+                <button onclick='deleteUser(${item.id})' class="w-full m-2 p-2 bg-blue-500 rounded text-white">DELETE</button>
+            </div>`;
+      });
+    });
+}
+function addUser() {
+  const userName = document.getElementById("name-input").value;
+  const userNID = document.getElementById("nid-input").value;
+  const userCity = document.getElementById("city-input").value;
+
+  fetch(`${baseURL}/user`, {
+    method: "POST",
+    headers: { "content-type": "application/JSON" },
+    body: JSON.stringify({
+      name: userName,
+      "national-id": userNID,
+      city: userCity,
+    }),
+  }).then((res) => {
+    res.ok && alert("User Sended");
+    renderUsers();
+  });
+}
+
+function deleteUser(id) {
+  fetch(`${baseURL}/user/${id}`, {
+    method: "DELETE",
+  }).then((res) => {
+    res.ok && alert(`User ${id} Deleted`);
+    renderUsers();
+  });
+}
